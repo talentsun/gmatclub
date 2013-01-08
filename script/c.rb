6725550ac63bab357cecfefa7f4636a78e4a222d
Jianfeng <tujf.cn@gmail.com>
2013-01-09 03:09:33 +0800

    use sqlite3

--- script/c.rb
+++ script/c.rb
@@ -1,7 +1,9 @@
 require 'net/http'
+require 'sqlite3'
 
 $url_prefix = "http://www.kaogmat.com/"
 $types = ['sc', 'cr', 'rc', 'ps', 'ds', 'ir', 'awa']
+$db_name = "result.sqlite"
 
 def get_url(type, page) 
   $url_prefix + type + '?p=' + page.to_s
@@ -11,6 +13,8 @@ def process_type_page(type, page)
   uri = URI(get_url(type, page))
   ret = Net::HTTP.get(uri)
   puts ret
+
+  SQLite3::Database.new($db_name)
 end
 
 def process_type(type) 
