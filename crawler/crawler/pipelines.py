18efeb6363fbb5e6788be5470882e24915f8af1a
Qiao Sun <sunqiao@wandoujia.com>
2013-01-16 02:00:25 +0800

    MOD: fix insert compound and argument error;

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -10,8 +10,11 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+import string
 import MySQLdb
 from items import Question, CompoundQuestion, Argument
+
+DB_PASSWORD = 'nameLR9969'
 
 class kaoGMATDBWriterPipeline(object):
     def process_item(self, item, spider):
@@ -51,7 +54,7 @@ class kaoGMATDBWriterPipeline(object):
             correct_rate = question['correct_rate'][0]
 
         try:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='nameLR9969', db='gmatclub', charset='utf8')
+            conn = MySQLdb.connect(host='localhost', user='root', passwd=DB_PASSWORD, db='gmatclub', charset='utf8')
         except Exception, e:
             print e
 
@@ -68,53 +71,78 @@ class kaoGMATDBWriterPipeline(object):
         cursor.close()
         conn.close()
     def insert_compound_question(self, compound_question):
-        kaogmat_id = compound_question['id']
+        kaogmat_id = compound_question['id'][0]
         print 'compound question #%s' % (kaogmat_id)
 
         type = compound_question['type']
         if type is None:
             type = 'other'
+        else:
+            type = compound_question['type'][0]
 
         set = compound_question['set']
         if set is None:
             set = 'other'
+        else:
+            set = compound_question['set'][0]
 
         rating = compound_question['rating']
         if rating is None:
             rating = 0.0
+        else:
+            rating = compound_question['rating'][0]
 
         correct_rate = compound_question['correct_rate']
         if correct_rate is None:
             correct_rate = 0.0
+        else:
+            correct_rate = compound_question['correct_rate'][0]
         
-        compound_question_id = str.join(compound_question['sub_questions'],'-')
+        compound_question_id = string.join(compound_question['sub_questions'],'-')
         
         try:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='nameLR9969', db='gmatclub', charset='utf8')
+            conn = MySQLdb.connect(host='localhost', user='root', passwd=DB_PASSWORD, db='gmatclub', charset='utf8')
         except Exception, e:
             print e
             
         try:
             cursor = conn.cursor()
             if not self.exists(compound_question_id):
-                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogmat_id, type, set, content, is_compound, is_sub) values (%s, %s, %s, %s, 1, 0, %s)', (compound_question_id, type, set, compound_question['article']))
+                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogmat_id, type, `set`, content, is_compound, is_sub) values(%s, %s, %s, %s, 1, 0)', (compound_question_id, type, set, compound_question['article'][0]))
                 conn.commit()
             else:
-                cursor.execute('update RAW_kaoGMAT_Questions set type=%s, `set`=%s, content=%s, is_compound=1, is_sub=0 where kaogmat_id=%s', (type, set, compound_question['article'], compound_question_id))
+                cursor.execute('update RAW_kaoGMAT_Questions set type=%s, `set`=%s, content=%s, is_compound=1, is_sub=0 where kaogmat_id=%s', (type, set, compound_question['article'][0], compound_question_id))
                 conn.commit()
         except Exception,e:
             print e
-        cursor.close()
+
+        content = compound_question['content']
+        if content is None:
+            content = ''
+        else:
+            content = compound_question['content'][0]
+            
+        answer = compound_question['answer']
+        if answer is None:
+            answer = ''
+        else:
+            answer = compound_question['answer'][0]
+            
+        explaination = compound_question['explaination']
+        if explaination is None:
+            explaination = ''
+        else:
+            explaination = compound_question['explaination'][0]
         
         try:
             cursor = conn.cursor()
-            if not self.exists(compound_question['id']):
-                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogmat_id, type, set, rating, content, answer, explaination, is_compound, is_sub, correct_rate) values(%s, %s, %s, %s, %s, %s, %s, 0, 1, %s)', (kaogmat_id, type, set, rating, compound_question['content'], compound_question['answer'], compound_question['explaination'], correct_rate))
+            if not self.exists(kaogmat_id):
+                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogmat_id, type, `set`, rating, content, answer, explaination, is_compound, is_sub, correct_rate) values(%s, %s, %s, %s, %s, %s, %s, 0, 1, %s)', (kaogmat_id, type, set, rating, content, answer, explaination, correct_rate))
                 conn.commit()
             else:
-                cursor.execute('update RAW_kaoGMAT_Questions set type=%s, `set`=%s, rating=%s, content=%s, answer=%s, explaination=%s, is_compound=0, is_sub=1, correct_rate=%s where kaogmat_id=%s', (type, set, rating, compound_question['content'], compound_question['answer'], compound_question['explaination'], correct_rate, compound_question['id']))
+                cursor.execute('update RAW_kaoGMAT_Questions set type=%s, `set`=%s, rating=%s, content=%s, answer=%s, explaination=%s, is_compound=0, is_sub=1, correct_rate=%s where kaogmat_id=%s', (type, set, rating, content, answer, explaination, correct_rate, kaogmat_id))
                 conn.commit()
-            insert_or_update_relationship(compound_question_id, compound_question['id'])
+            self.insert_or_update_relationship(compound_question_id, kaogmat_id)
         except Exception, e:
             print e
         
@@ -122,21 +150,31 @@ class kaoGMATDBWriterPipeline(object):
         conn.close()
     def insert_argument(self,argument):
         try:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='nameLR9969', db='gmatclub', charset='utf8')
+            conn = MySQLdb.connect(host='localhost', user='root', passwd=DB_PASSWORD, db='gmatclub', charset='utf8')
         except Exception,e:
             print e
         
-        rating = argument[rating]
+        kaogmat_id = argument['id'][0]
+        
+        rating = argument['rating']
         if rating is None:
             rating = 0.0
+        else:
+            rating = argument['rating'][0]
+        
+        essay = argument['essay']
+        if essay is None:
+            essay = ''
+        else:
+            essay = argument['essay'][0]
         
         try:
             cursor = conn.cursor()
-            if not self.exists(argument['id']):
-                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogamt_id, rating, essay) values(%s, %s, %s)', (argument['id'], rating, argument['essay']))
+            if not self.exists(kaogmat_id):
+                cursor.execute('insert into RAW_kaoGMAT_Questions(kaogmat_id, rating, content) values(%s, %s, %s)', (kaogmat_id, rating, essay))
                 conn.commit()
             else:
-                cursor.execute('update RAW_kaoGMAT_Questions set rating=%s, essay=%s where kaogmat_id=', (rating, argument['essay'], argument['id']))
+                cursor.execute('update RAW_kaoGMAT_Questions set rating=%s, content=%s where kaogmat_id=%s', (rating, essay, kaogmat_id))
                 conn.commit()
         except Exception, e:
             print e
@@ -145,7 +183,7 @@ class kaoGMATDBWriterPipeline(object):
         conn.close()
     def exists(self, question_id):
         try:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='nameLR9969', db='gmatclub', charset='utf8')
+            conn = MySQLdb.connect(host='localhost', user='root', passwd=DB_PASSWORD, db='gmatclub', charset='utf8')
         except Exception, e:
             print e
             
@@ -163,7 +201,7 @@ class kaoGMATDBWriterPipeline(object):
         return exist
     def insert_or_update_relationship(self, compound_question_id, sub_question_id):
         try:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='nameLR9969', db='gmatclub', charset='utf8')
+            conn = MySQLdb.connect(host='localhost', user='root', passwd=DB_PASSWORD, db='gmatclub', charset='utf8')
         except Exception,e:
             print e
         
